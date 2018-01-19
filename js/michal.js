d05aa7cf4fa5b3806911235f02a6944ab7e15b8e
RANF <frajman.ran@gmail.com>
2018-01-18 19:22:45 -0500

    Words for Michal in colors

--- js/michal.js
+++ js/michal.js
@@ -1,28 +1,107 @@
-var _words = [
-    "I", "a", "the",
-    "to", "said", "is",
-    "was", "you", "are",
-    "have", "has", "of",
-    "do", "come", "from",
-    "and", "good", "little",
-    "were", "want", "your",
-    "our", "says", "they",
-    "because", "goes", "don't",
-    "who", "what", "where",
-    "any", "many", "funny",
-    "live", "give", "love",
-    "could", "should", "would",
-    "does", "off", "put",
-    "pretty", "busy", "friend",
-    "walk", "talk", "chalk",
-    "some", "been", "gone",
-    "heart", "people", "buy",
-    "done", "once", "there",
-    "sure", "sugar", "their",
-    "heart", "thought", "whose",
-    "again", "against", "build",
-    "though", "enough", "through"
-]
+var AppData = function() {
+    this.pages = [{
+            header: '1',
+            color: 'blue',
+            words: [
+                "I",
+                "a",
+                "the",
+                "to",
+                "said",
+                "is",
+                "was",
+                "you",
+                "are",
+                "have",
+                "has",
+                "of",
+                "do",
+                "come",
+                "from"
+            ]
+        },
+        {
+            header: '2',
+            color: '#FFE200',
+            words: [
+                "and",
+                "good",
+                "little",
+                "were",
+                "want",
+                "your",
+                "our",
+                "says",
+                "they",
+                "because",
+                "goes",
+                "don't",
+                "who",
+                "what",
+                "where"
+            ]
+        },
+        {
+            header: '3',
+            color: 'red',
+            words: [
+                "any",
+                "many",
+                "funny",
+                "live",
+                "give",
+                "love",
+                "could",
+                "should",
+                "would",
+                "does",
+                "off",
+                "put",
+                "pretty",
+                "busy",
+                "friend"
+            ]
+        },
+        {
+            header: '4',
+            color: 'green',
+            words: [
+                "walk",
+                "talk",
+                "chalk",
+                "some",
+                "been",
+                "gone",
+                "heart",
+                "people",
+                "buy",
+                "done",
+                "once",
+                "there",
+                "sure",
+                "sugar",
+                "their"
+            ]
+        },
+        {
+            header: '5',
+            color: 'gray',
+            words: [
+                "heart",
+                "thought",
+                "whose",
+                "again",
+                "against",
+                "build",
+                "though",
+                "enough",
+                "through"
+            ]
+        }
+    ];
+};
+
+var data = new AppData();
 
 function fillWords() {
     var container = $(".container-fluid");
@@ -35,20 +114,40 @@ function fillWords() {
 }
 
 function playWord(button) {
-    var isPlaying = button.data("status") === "playing";
+    $button = $(button);
+    var isPlaying = $button.data("status") === "playing";
     if (!isPlaying) {
-        var word = button.text();
-        button.html("<img src='img/play.png' />");
-        button.data("status", "playing")
+        var word = $button.text();
+        $button.html("<img src='img/play.png' />");
+        $button.data("status", "playing")
         responsiveVoice.speak(word);
         setTimeout(function() {
-            button.html(word);
-            button.data("status", "ready");
+            $button.html(word);
+            $button.data("status", "ready");
         }, 500);
     }
 }
 
+function initHeader() {
+    var source = document.getElementById("pages-template").innerHTML;
+    var template = Handlebars.compile(source)(data);
+    $(".pages-headers").html(template);
+}
+
+function initPage(page) {
+    var source = document.getElementById("words-template").innerHTML;
+    var template = Handlebars.compile(source)(page);
+    $(".work-place").html(template).scrollTop(0);
+}
+
+function activate(index) {
+    var page = data.pages[index];
+    $(".work-place").css("background-color", page.color).show();
+    initPage(page);
+}
+
 $(document).ready(function() {
     responsiveVoice.setDefaultVoice("US English Female");
-    fillWords();
+    initHeader();
+    activate(0);
 });
