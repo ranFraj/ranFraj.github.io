584f2bdfecf18a19ee563408a04f1164339fcbbb
Frajman, Ran (rf392f) <rf392f@att.com>
2019-07-23 16:24:51 -0400

    components mechanism back compatible

--- Seeds/app/components/main/script.js
+++ Seeds/app/components/main/script.js
@@ -1,4 +1,9 @@
 app.current.code = {
+    next: () => {
+        const component = new Component('upload', './components/upload');
+        app.setComponent(component);
+        app.run();
+    },
     run: () => {
         console.log('main running');
     }
--- Seeds/app/js/app.js
+++ Seeds/app/js/app.js
@@ -14,18 +14,46 @@ class App {
     }
 
     back() {
-        const last = () => { return this.components.length - 1 };
-        this.current.removeCSS()
-        this.components.splice(last(), 1);
-        this.current = this.components[last()];
-        this.container.html(this.current.html);
+        let count = this.container.find('.component').length;
+        const removeLast = (c) => {
+            $(c).remove();
+            this.current.removeCSS()
+            this.components.splice(count - 1, 1);
+            count--;
+            this.current = this.components[count - 1];
+        }
+        setTimeout(() => {
+            this.container.find('.component').each((i, c) => {
+                const left = parseInt(c.style.left) + 100;
+                c.style.left = left + '%';
+                if (i === count - 1) {
+                    setTimeout(() => {
+                        removeLast(c);
+                    }, 500);
+                }
+            });
+        }, 100);
     }
 
     run() {
         this.current.init(() => {
+            let count = this.container.find('.component').length;
             this.container.attr('data-component', this.current.name);
-            this.container.html(this.current.html);
+            if (count > 0) {
+                this.container.append(this.current.html);
+            } else {
+                this.container.html(this.current.html);
+            }
+            count++;
             this.current.code.run();
-        })
+            if (count > 1) {
+                setTimeout(() => {
+                    this.container.find('.component').each((i, c) => {
+                        const left = (count - 1 - i) * 100;
+                        c.style.left = (left * -1) + '%';
+                    });
+                }, 100);
+            }
+        });
     }
 }
