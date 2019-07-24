9599b77593fa4316ab134404111e1c19b9f4c56a
Frajman, Ran (rf392f) <rf392f@att.com>
2019-07-24 11:53:44 -0400

    Hamburger menu

--- Seeds/app/components/main/script.js
+++ Seeds/app/components/main/script.js
@@ -1,8 +1,22 @@
 app.current.code = {
-    next: () => {
+    /*next: () => {
         const component = new Component('upload', './components/upload');
         app.setComponent(component);
         app.run();
+    },*/
+    menuBackClick: (e) => {
+        $('.menu-back').hide();
+    },
+    menuClick: (e) => {
+        e.stopPropagation();
+    },
+    toggleMenu: () => {
+        const menu = $('.menu-back');
+        if (menu.is(':visible')) {
+            menu.hide();
+        } else {
+            menu.show();
+        }
     },
     run: () => {
         console.log('main running');
